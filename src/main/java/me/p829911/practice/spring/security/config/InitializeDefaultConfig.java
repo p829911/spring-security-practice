c5cafdd3947e54fc75bda5329b9f45fc1c0c2edf
seungwoo.hyun <p829911@gmail.com>
2022-04-18 20:19:31 +0900

    Add config class

---
+++ src/main/java/me/p829911/practice/spring/security/config/InitializeDefaultConfig.java
@@ -0,0 +1,46 @@
+package me.p829911.practice.spring.security.config;
+
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import me.p829911.practice.spring.security.note.NoteService;
+import me.p829911.practice.spring.security.notice.NoticeService;
+import me.p829911.practice.spring.security.user.User;
+import me.p829911.practice.spring.security.user.UserService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * 초기 상태 등록 Config
+ */
+@Configuration
+@RequiredArgsConstructor
+@Profile(value = "!test")
+public class InitializeDefaultConfig {
+
+    private final UserService userService;
+    private final NoteService noteService;
+    private final NoticeService noticeService;
+
+    /**
+     * 유저등록, note 4개 등록
+     */
+    @PostConstruct
+    public void initializeDefaultUser() {
+        User user = userService.signup("user", "user");
+        noteService.saveNote(user, "테스트", "테스트입니다.");
+        noteService.saveNote(user, "테스트2", "테스트2입니다.");
+        noteService.saveNote(user, "테스트3", "테스트3입니다.");
+        noteService.saveNote(user, "여름 여행계획", "여름 여행계획 작성중...");
+    }
+
+    /**
+     * 어드민등록, 공지사항 2개 등록
+     */
+    @PostConstruct
+    public void initializeDefaultAdmin() {
+        userService.signupAdmin("admin", "admin");
+        noticeService.saveNotice("환영합니다.", "환영합니다 여러분");
+        noticeService.saveNotice("노트 작성 방법 공지",
+            "1. 회원가입\n2. 로그인\n3. 노트 작성\n4. 저장\n* 본인 외에는 게시글을 볼 수 없습니다.");
+    }
+}
